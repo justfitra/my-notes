fa934d466d6f2d9eee1973b0bb3b3dc164eec159
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-09-30 19:31:11 +0700

    add new feat : modal to add notes

--- script/modal.js
+++ script/modal.js
@@ -1,1 +1,12 @@
-export const Modal = () => {};
+const modal = document.getElementById("modal");
+const sidebar = document.getElementById("sidebar");
+const openModal = document.getElementById("addNote");
+const closeModal = document.getElementById("close");
+
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+openModal.addEventListener("click", function () {
+  modal.style.display = "block";
+});
