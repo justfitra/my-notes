7b4263ea1b7743c4054722fdf3d5e1e57c94f0d1
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-10-05 21:16:01 +0700

    repair feat : add note

--- api/postNote.js
+++ api/postNote.js
@@ -1,15 +1,27 @@
-import { API_KEY } from "../env.js";
-async function postNote(e, titleNote, contentNote) {
+import { URL } from "../env.js";
+async function postNote(titleNote, contentNote) {
   try {
+    const id = await fetch(`${URL}`);
+    const idData = await id.json();
+
+    const newId = idData.length + 1;
+
     const formInput = {
+      id: newId,
       title: titleNote,
       content: contentNote,
+      date: new Date().toLocaleString(),
     };
-    const data = await fetch(`${API_KEY}?action=create`, {
+
+    const data = await fetch(`${URL}`, {
       method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(formInput),
     });
-    // this.reset();
+
     return data;
   } catch (err) {
     console.log(err.message);
