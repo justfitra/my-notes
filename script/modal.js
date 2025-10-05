5f9ce800369ed6aa263cdb2e1f16b2e34d2e9f67
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-10-05 21:16:46 +0700

    add new modal fn

--- script/modal.js
+++ script/modal.js
@@ -1,9 +1,11 @@
 const modal = document.getElementById("modal");
 const showModal = document.getElementById("show-modal");
+const editModal = document.getElementById("editModal");
 const sidebar = document.getElementById("sidebar");
 const openModal = document.getElementById("addNote");
 const closeModal = document.getElementById("close");
 const closeShowModal = document.getElementById("show-close");
+const closeEditModal = document.getElementById("edit-close");
 
 closeModal.onclick = function () {
   modal.style.display = "none";
@@ -13,15 +15,24 @@ closeShowModal.onclick = function () {
   showModal.style.display = "none";
 };
 
+closeEditModal.onclick = function () {
+  editModal.style.display = "none";
+};
+
 openModal.addEventListener("click", function () {
   modal.style.display = "block";
 });
 
 window.onclick = function (event) {
-  if (event.target == modal || event.target == showModal) {
+  if (
+    event.target == modal ||
+    event.target == showModal ||
+    event.target == editModal
+  ) {
     modal.style.display = "none";
+    editModal.style.display = "none";
     showModal.style.display = "none";
   }
 };
 
-console.log(showModal);
+console.log(editModal);
